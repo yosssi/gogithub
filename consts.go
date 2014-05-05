10f33d74f4599019020fe9e389959ea9d3389c3d
yosssi <yoshida.keiji.84@gmail.com>
2014-05-05 23:07:51 +0900

    Update consts.go.

--- consts.go
+++ consts.go
@@ -1,22 +1,14 @@
 package gogithub
 
+// consts definition
 const (
-	// NormalURL represents a GitHub normal URL.
-	NormalURL = "https://github.com"
-	// APIURL represents a GitHub API URL.
-	APIURL = "https://api.github.com"
-	// AccessTokenURL represents an OAuth access token API URL.
-	AccessTokenURL = NormalURL + "/login/oauth/access_token"
-	// AuthenticatedUserURL represents a get user API URL.
-	AuthenticatedUserURL = APIURL + "/user"
-	// URLParamPrefix represents a URL parameter prefix.
-	URLParamPrefix = "?"
-	// ParamKeyAccessToken represents a parameter key of an access token.
-	ParamKeyAccessToken = "access_token"
-	// GetContentsURL represents a get contents API URL.
-	GetContentsURL = APIURL + "/repos/%s/%s/contents/%s"
-	// GetContentURL represents a get content API URL.
-	GetContentURL = "https://raw.githubusercontent.com/%s/%s/%s/%s"
-	// SearchRepositoriesURL represents a search repositories API URL.
+	NormalURL             = "https://github.com"
+	APIURL                = "https://api.github.com"
+	AccessTokenURL        = NormalURL + "/login/oauth/access_token"
+	AuthenticatedUserURL  = APIURL + "/user"
+	URLParamPrefix        = "?"
+	ParamKeyAccessToken   = "access_token"
+	GetContentsURL        = APIURL + "/repos/%s/%s/contents/%s"
+	GetContentURL         = "https://raw.githubusercontent.com/%s/%s/%s/%s"
 	SearchRepositoriesURL = APIURL + "/search/repositories"
 )
